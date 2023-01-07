8fca1360e5fbb42fabb55f3d1767ddbd26bc3597
yasinakcadev <yasinakcadev@gmail.com>
2023-01-07 10:58:39 +0300

    first commit

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOSMVVM
+//
+//  Created by YASIN on 7.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
